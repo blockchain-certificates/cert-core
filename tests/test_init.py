5cefe7fc60024f5335e38e2e1006001381038d42
kimdhamilton <kimdhamilton@gmail.com>
2018-04-15 12:59:48 -0700

    update dependencies, fix test warnings, and remove dependency on parse_requirements

--- tests/test_init.py
+++ tests/test_init.py
@@ -14,83 +14,83 @@ class TestInit(unittest.TestCase):
 
     def test_blockchain_display_value_bitcoin(self):
         chain = BlockchainType.bitcoin
-        self.assertEquals(chain.external_display_value, 'BTCOpReturn')
+        self.assertEqual(chain.external_display_value, 'BTCOpReturn')
 
     def test_blockchain_display_value_ethereum(self):
         chain = BlockchainType.ethereum
-        self.assertEquals(chain.external_display_value, 'ETHData')
+        self.assertEqual(chain.external_display_value, 'ETHData')
 
     def test_blockchain_display_value_mock(self):
         chain = BlockchainType.mock
-        self.assertEquals(chain.external_display_value, 'Mock')
+        self.assertEqual(chain.external_display_value, 'Mock')
 
     def test_parse_from_chain_string_bitcoin_mainnet(self):
         chain = Chain.parse_from_chain('bitcoin_mainnet')
-        self.assertEquals(chain, Chain.bitcoin_mainnet)
+        self.assertEqual(chain, Chain.bitcoin_mainnet)
 
     def test_parse_from_chain_string_bitcoin_testnet(self):
         chain = Chain.parse_from_chain('bitcoin_testnet')
-        self.assertEquals(chain, Chain.bitcoin_testnet)
+        self.assertEqual(chain, Chain.bitcoin_testnet)
 
     def test_parse_from_chain_string_bitcoin_regtest(self):
         chain = Chain.parse_from_chain('bitcoin_regtest')
-        self.assertEquals(chain, Chain.bitcoin_regtest)
+        self.assertEqual(chain, Chain.bitcoin_regtest)
 
     def test_parse_from_chain_string_ethereum_mainnet(self):
         chain = Chain.parse_from_chain('ethereum_mainnet')
-        self.assertEquals(chain, Chain.ethereum_mainnet)
+        self.assertEqual(chain, Chain.ethereum_mainnet)
 
     def test_parse_from_chain_string_ethereum_testnet(self):
         chain = Chain.parse_from_chain('ethereum_testnet')
-        self.assertEquals(chain, Chain.ethereum_testnet)
+        self.assertEqual(chain, Chain.ethereum_testnet)
 
     def test_parse_from_chain_string_ethereum_ropsten(self):
         chain = Chain.parse_from_chain('ethereum_ropsten')
-        self.assertEquals(chain, Chain.ethereum_ropsten)
+        self.assertEqual(chain, Chain.ethereum_ropsten)
 
     def test_parse_from_chain_string_mockchain(self):
         chain = Chain.parse_from_chain('mockchain')
-        self.assertEquals(chain, Chain.mockchain)
+        self.assertEqual(chain, Chain.mockchain)
 
     def test_parse_from_external_display_value_bitcoin_mainnet(self):
         chain = Chain.parse_from_external_display_value('bitcoinMainnet')
-        self.assertEquals(chain, Chain.bitcoin_mainnet)
+        self.assertEqual(chain, Chain.bitcoin_mainnet)
 
     def test_parse_from_external_display_value_bitcoin_testnet(self):
         chain = Chain.parse_from_external_display_value('bitcoinTestnet')
-        self.assertEquals(chain, Chain.bitcoin_testnet)
+        self.assertEqual(chain, Chain.bitcoin_testnet)
 
     def test_parse_from_external_display_value_bitcoin_regtest(self):
         chain = Chain.parse_from_external_display_value('bitcoinRegtest')
-        self.assertEquals(chain, Chain.bitcoin_regtest)
+        self.assertEqual(chain, Chain.bitcoin_regtest)
 
     def test_parse_from_external_display_value_ethereum_mainnet(self):
         chain = Chain.parse_from_external_display_value('ethereumMainnet')
-        self.assertEquals(chain, Chain.ethereum_mainnet)
+        self.assertEqual(chain, Chain.ethereum_mainnet)
 
     def test_parse_from_external_display_value_ethereum_testnet(self):
         chain = Chain.parse_from_external_display_value('ethereumTestnet')
-        self.assertEquals(chain, Chain.ethereum_testnet)
+        self.assertEqual(chain, Chain.ethereum_testnet)
 
     def test_parse_from_external_display_value_ethereum_ropsten(self):
         chain = Chain.parse_from_external_display_value('ethereumRopsten')
-        self.assertEquals(chain, Chain.ethereum_ropsten)
+        self.assertEqual(chain, Chain.ethereum_ropsten)
 
     def test_parse_from_external_display_value_mockchain(self):
         chain = Chain.parse_from_external_display_value('mockchain')
-        self.assertEquals(chain, Chain.mockchain)
+        self.assertEqual(chain, Chain.mockchain)
 
     def test_bitcoin_chain_to_netcode_bitcoin_mainnet(self):
         netcode = chain_to_bitcoin_network(Chain.bitcoin_mainnet)
-        self.assertEquals(netcode, 'mainnet')
+        self.assertEqual(netcode, 'mainnet')
 
     def test_bitcoin_chain_to_netcode_bitcoin_testnet(self):
         netcode = chain_to_bitcoin_network(Chain.bitcoin_testnet)
-        self.assertEquals(netcode, 'testnet')
+        self.assertEqual(netcode, 'testnet')
 
     def test_bitcoin_chain_to_netcode_bitcoin_testnet(self):
         netcode = chain_to_bitcoin_network(Chain.bitcoin_regtest)
-        self.assertEquals(netcode, 'regtest')
+        self.assertEqual(netcode, 'regtest')
 
     def test_bitcoin_chain_to_netcode_mocknet(self):
         """
@@ -116,7 +116,7 @@ class TestInit(unittest.TestCase):
 
     def test_blockcerts_versions_v2(self):
         v2_alpha = BlockcertVersion.V2_ALPHA
-        self.assertEquals(v2_alpha, BlockcertVersion.V2_ALPHA)
+        self.assertEqual(v2_alpha, BlockcertVersion.V2_ALPHA)
 
 if __name__ == '__main__':
     unittest.main()
